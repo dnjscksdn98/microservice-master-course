295bb8099472d35b48e31a65e8c4ae90a12d7cf4
Alex Won <dnjscksdn98@gmail.com>
2020-07-27 13:38:27 +0900

    Create: create eureka discovery service

---
+++ PhotoAppDiscoveryService/src/main/java/com/alexcode/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexcode.discovery.PhotoAppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+	}
+
+}
