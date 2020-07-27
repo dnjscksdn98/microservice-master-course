ea877931823eb0359dcaa9967644d7e3388af2bb
Alex Won <dnjscksdn98@gmail.com>
2020-07-27 14:41:21 +0900

    Add: add eureka client configurations

--- PhotoAppApiUsers/src/main/java/com/alexcode/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/alexcode/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -2,8 +2,10 @@ package com.alexcode.photoapp.api.users.PhotoAppApiUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
