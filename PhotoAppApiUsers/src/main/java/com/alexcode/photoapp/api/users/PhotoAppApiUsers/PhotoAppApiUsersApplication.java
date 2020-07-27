3c02ea7508febc189afe53fad82d4289d02d50ac
Alex Won <dnjscksdn98@gmail.com>
2020-07-27 14:24:15 +0900

    Create: create user micro service

---
+++ PhotoAppApiUsers/src/main/java/com/alexcode/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.alexcode.photoapp.api.users.PhotoAppApiUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+
+}
---
+++ PhotoAppApiUsers/src/test/java/com/alexcode/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexcode.photoapp.api.users.PhotoAppApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
