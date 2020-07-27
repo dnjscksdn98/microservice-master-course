7c62f48683509c1fbddf617ec324ff5ae89b4e0e
Alex Won <dnjscksdn98@gmail.com>
2020-07-27 13:56:55 +0900

    Add: add initial configuration for eureka service

--- PhotoAppDiscoveryService/src/main/java/com/alexcode/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/alexcode/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.alexcode.discovery.PhotoAppDiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
