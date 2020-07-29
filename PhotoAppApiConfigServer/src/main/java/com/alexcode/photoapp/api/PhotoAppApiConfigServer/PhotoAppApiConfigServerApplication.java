4235747285807ed8fc2885c920ff309711d711a3
Alex Won <dnjscksdn98@gmail.com>
2020-07-29 13:35:55 +0900

    Create: create spring config server

---
+++ PhotoAppApiConfigServer/src/main/java/com/alexcode/photoapp/api/PhotoAppApiConfigServer/PhotoAppApiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.alexcode.photoapp.api.PhotoAppApiConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class PhotoAppApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ PhotoAppApiConfigServer/src/test/java/com/alexcode/photoapp/api/PhotoAppApiConfigServer/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexcode.photoapp.api.PhotoAppApiConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
