1f496ab09755a6cf8afca405a0e4f70ce9a5e579
Alex Won <dnjscksdn98@gmail.com>
2020-08-02 16:29:06 +0900

    Add: trace requests with Sleuth and Zipkin

--- PhotoAppApiAlbums/src/main/java/com/alexcode/photoapp/api/albums/PhotoAppApiAlbums/service/AlbumServiceImpl.java
+++ PhotoAppApiAlbums/src/main/java/com/alexcode/photoapp/api/albums/PhotoAppApiAlbums/service/AlbumServiceImpl.java
@@ -13,28 +13,27 @@ public class AlbumServiceImpl implements AlbumService{
 
     @Override
     public List<AlbumEntity> getAlbums(String userId) {
-        throw new UserNotFoundException(userId);
-//        List<AlbumEntity> albumEntities = new ArrayList<>();
-//
-//        AlbumEntity albumEntity = AlbumEntity.builder()
-//                .id(1L)
-//                .userId(userId)
-//                .albumId(UUID.randomUUID().toString())
-//                .name("test album 1")
-//                .description("test album 1 description")
-//                .build();
-//
-//        AlbumEntity albumEntity2 = AlbumEntity.builder()
-//                .id(2L)
-//                .userId(userId)
-//                .albumId(UUID.randomUUID().toString())
-//                .name("test album 2")
-//                .description("test album 2 description")
-//                .build();
-//
-//        albumEntities.add(albumEntity);
-//        albumEntities.add(albumEntity2);
-//
-//        return albumEntities;
+        List<AlbumEntity> albumEntities = new ArrayList<>();
+
+        AlbumEntity albumEntity = AlbumEntity.builder()
+                .id(1L)
+                .userId(userId)
+                .albumId(UUID.randomUUID().toString())
+                .name("test album 1")
+                .description("test album 1 description")
+                .build();
+
+        AlbumEntity albumEntity2 = AlbumEntity.builder()
+                .id(2L)
+                .userId(userId)
+                .albumId(UUID.randomUUID().toString())
+                .name("test album 2")
+                .description("test album 2 description")
+                .build();
+
+        albumEntities.add(albumEntity);
+        albumEntities.add(albumEntity2);
+
+        return albumEntities;
     }
 }
