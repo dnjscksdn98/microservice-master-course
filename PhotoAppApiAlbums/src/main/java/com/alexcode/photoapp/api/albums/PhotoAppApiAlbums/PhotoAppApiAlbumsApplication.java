0f6eb7f8fd27bac2781ebf8f54cd8eaf8c6a77f2
Alex Won <dnjscksdn98@gmail.com>
2020-08-04 21:06:06 +0900

    Create: build dockerfile for albums microservice

--- PhotoAppApiAlbums/src/main/java/com/alexcode/photoapp/api/albums/PhotoAppApiAlbums/PhotoAppApiAlbumsApplication.java
+++ PhotoAppApiAlbums/src/main/java/com/alexcode/photoapp/api/albums/PhotoAppApiAlbums/PhotoAppApiAlbumsApplication.java
@@ -2,8 +2,10 @@ package com.alexcode.photoapp.api.albums.PhotoAppApiAlbums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiAlbumsApplication {
 
 	public static void main(String[] args) {
