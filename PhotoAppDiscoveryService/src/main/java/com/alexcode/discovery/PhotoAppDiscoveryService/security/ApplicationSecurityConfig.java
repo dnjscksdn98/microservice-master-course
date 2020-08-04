8753e39dcdc1a21777d19a2e5c710ae233d62e6e
Alex Won <dnjscksdn98@gmail.com>
2020-08-04 10:01:17 +0900

    Update: secure eureka discovery service

---
+++ PhotoAppDiscoveryService/src/main/java/com/alexcode/discovery/PhotoAppDiscoveryService/security/ApplicationSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.alexcode.discovery.PhotoAppDiscoveryService.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
